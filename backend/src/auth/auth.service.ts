2a613a4ff4e2e2611fe93ba43aaeea11e2e13c2e
Fernandajo <fernandajoesting@gmail.com>
2025-12-30 16:34:21 +0100

    Feat: password hashing implementation

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
-    signup(dto: AuthDto) {
-        console.log(dto);
+    async signup(dto: AuthDto) {
+    //generate
+        const hash = await argon.hash(dto.password);
+
+        const user = await  this.prisma.user.create({
+            data: {
+                email: dto.email,
+                password: hash,
+            },
+        });
+        return user;
     }
 
     signin() {
