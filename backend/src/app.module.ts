d0ef7f9dc8f1034bce26e9271426c2ba9169673e
mdomnik <mdomnik.maciej@gmail.com>
2026-01-02 22:34:39 +0100

    Refactor: removed unnecesary imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
-import { quizController } from './quiz/quiz.controller';
-import { quizService } from './quiz/quiz.service';
 import { quizModule } from './quiz/quiz.module';
 import { ConfigModule } from '@nestjs/config';
 
