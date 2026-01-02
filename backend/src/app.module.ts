02a641975f4596ae222d40eb1060fbcb234155c3
mdomnik <mdomnik.maciej@gmail.com>
2026-01-02 22:08:32 +0100

    Feat: Set up ConfigModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { quizController } from './quiz/quiz.controller';
+import { quizService } from './quiz/quiz.service';
+import { quizModule } from './quiz/quiz.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule, AuthModule, quizModule],
   controllers: [AppController],
   providers: [AppService],
 })
