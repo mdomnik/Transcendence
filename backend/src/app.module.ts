1056a3829ad55debbb2587abf6a4bf94953d61bf
Fernandajo <fernandajoesting@gmail.com>
2026-01-02 23:52:52 +0100

    Feat: implemented user controllers and authGuards

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [PrismaModule, AuthModule],
-  controllers: [AppController],
+  imports: [ConfigModule.forRoot({isGlobal: true}), PrismaModule, AuthModule],
+  controllers: [AppController, UserController],
   providers: [AppService],
 })
 export class AppModule {}
