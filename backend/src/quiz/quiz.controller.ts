a8ece4fdae2c9f8489b49e12d1be696d2892cde9
mdomnik <mdomnik.maciej@gmail.com>
2026-01-02 22:35:04 +0100

    refactor: quiz service renamed

--- backend/src/quiz/quiz.controller.ts
+++ backend/src/quiz/quiz.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Post, Query } from '@nestjs/common';
 import { TopicDto } from './dto';
 import { json } from 'stream/consumers';
-import { quizService } from './quiz.service';
+import { QuizService } from './quiz.service';
 
 @Controller('quiz')
 export class quizController {
     constructor(
-        private readonly quizService: quizService,
+        private readonly quizService: QuizService,
     ) {}
 
     @Get('questions')
