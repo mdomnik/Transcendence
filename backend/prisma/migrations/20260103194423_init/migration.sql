c93f4bec5390daa65f48f64ec4d656c2d1900665
mdomnik <mdomnik.maciej@gmail.com>
2026-01-03 20:46:51 +0100

    Fix: Fixed divergent database migrations

--- backend/prisma/migrations/20260103194423_init/migration.sql
+++ backend/prisma/migrations/20260103194423_init/migration.sql
@@ -2,9 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "firstName" TEXT,
     "password" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -13,6 +11,3 @@ CREATE TABLE "User" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
