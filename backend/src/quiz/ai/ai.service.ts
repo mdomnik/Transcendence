5a88275b3d64829b2e4bb403fe79743d6ef03014
mdomnik <mdomnik.maciej@gmail.com>
2026-01-06 00:37:28 +0100

    Chore: moved generateQuestions to the AIService

--- backend/src/quiz/ai/ai.service.ts
+++ backend/src/quiz/ai/ai.service.ts
@@ -3,13 +3,39 @@ import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { AiResponseException } from 'src/common/exceptions/ai-response.exception';
 import { aiLimiter } from './ai.limiter';
+import { QuestionDto, TopicDto } from '../dto';
+import { QuizPromptBuilder } from '../prompt/quiz.prompt.builder';
+import { QuizResponseParser } from '../parser/quiz.response.parser';
 
 @Injectable()
 export class AiService {
     constructor(
         private readonly httpService: HttpService,
         private readonly configService: ConfigService,
+        private readonly promptBuilder: QuizPromptBuilder,
+        private readonly parser: QuizResponseParser,
     ) { }
+    async generateQuestions(dto: TopicDto, excludeQuestions: string[], ): Promise<QuestionDto[]> {
+
+        const formattedExclusionQuestions = excludeQuestions.join('\n');
+
+        const payload = this.promptBuilder.buildPayload(
+            dto,
+            this.configService.getOrThrow('AI_MODEL'),
+            formattedExclusionQuestions,
+        );
+
+        console.log(`excludeQuestions: ${formattedExclusionQuestions}`);
+
+        const response = await this.send(payload);
+
+        const generatedArray = await this.parser.parse(
+            response.choices[0].message.content,
+            dto.difficulty,
+        );
+
+        return generatedArray;
+    }
 
     async send(payload: unknown): Promise<any> {
         try {
