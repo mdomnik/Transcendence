9421330440d658ec2625c7492fcf11e7e89e46b3
Fernandajo <fernandajoesting@gmail.com>
2026-01-06 11:19:11 +0100

    Feat: implemented google strategy and added username and googleId to database

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { GoogleAuthGuard } from './strategy/Guards';
+import type { Request, Response } from 'express';
+import { User } from 'src/common/decorators/user.decorator';
+
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +18,29 @@ export class AuthController {
     @Post('signin')
     signin(@Body() dto: AuthDto) {
         return this.authService.signin(dto);
+    }
+
+    @Get('google/login')
+    @UseGuards(GoogleAuthGuard)
+    handleLogin(){
+
+    }
+
+    @Get('google/redirect')
+    @UseGuards(GoogleAuthGuard)
+    async handleRedirect(@User() user, @Res() res: Response) {
+        // const user = await this.authService.handleGoogleLogin(googleUser);
+        const accessToken = await this.authService.signToken(user.id, user.email);
+        res.cookie('access_token', accessToken, {
+          httpOnly: true,
+          sameSite: 'lax',
+          secure: false, // true in production
+          maxAge: 15 * 60 * 1000,
+        });
+        console.log("access token: ", accessToken)
+        // TO DO 
+        // properly connect with front end
+        //access token has to work
+        return res.redirect('http://localhost:8080/dashboard');
     }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './strategy';
+import { GoogleStrategy } from './strategy/google.strategy';
 
 @Module({
   imports: [JwtModule.register({})],
-  providers: [AuthService, JwtStrategy],
+  providers: [AuthService, JwtStrategy, GoogleStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
