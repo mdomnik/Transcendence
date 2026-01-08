8c65d2d42c9740083e8720a1cb77b4ab6a7b4203
mdomnik <mdomnik.maciej@gmail.com>
2026-01-08 21:13:39 +0100

    docs: added comments

--- backend/src/quiz/quiz.controller.ts
+++ backend/src/quiz/quiz.controller.ts
@@ -5,14 +5,14 @@ import { Throttle } from '@nestjs/throttler';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from 'src/common/decorators/user.decorator';
 
+// Endpoint of the quiz module used for quiz related functionality
 @Controller('quiz')
 export class QuizController {
-  constructor(
-    private readonly quizService: QuizService,
-  ) { }
+  constructor(private readonly quizService: QuizService) { }
 
-  @UseGuards(AuthGuard('jwt'))
-  @Throttle({ ai: { limit: 5, ttl: 60000 } })
+  // GET for (/api/quiz/questions) endpoint which takes a query of ({topic=x&qnum=x&&difficulty=x})
+  @UseGuards(AuthGuard('jwt')) // Guard Requiring jwt authentication for call
+  @Throttle({ ai: { limit: 5, ttl: 60000 } }) // Throttler that limits ip to max 5 calls per minute
   @Get('questions')
   async getQuestions(
     @Query() query: TopicDto,
