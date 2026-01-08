4dfd10848d521d23ca316bc9c2677393e4d612a2
Fernandajo <fernandajoesting@gmail.com>
2026-01-08 19:39:07 +0100

    Feat: implemented ssl and https everywhere

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,8 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
 
   app.useGlobalPipes(new ValidationPipe({
       whitelist: true,
@@ -11,6 +13,6 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('api');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
