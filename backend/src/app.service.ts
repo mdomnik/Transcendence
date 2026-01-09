dfdfe1c1fa0633ba3aa866b40706aefa48ae0ca4
mdomnik <mdomnik.maciej@gmail.com>
2026-01-09 16:31:55 +0100

    Feat: rerouted backend root to simple status check

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,15 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World, This is a testaasd!';
-  }
-
-  getTest(): string {
-    return 'testing';
-  }
-
-  getData(): string {
-    return 'data';
+  getConnectionStatus(): string {
+    return 'Backend Communication Established. Hi!';
   }
 }
