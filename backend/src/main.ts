78f16c7a0af72dc46f4a7722ba6d26ee23d7b3fc
mdomnik <mdomnik.maciej@gmail.com>
2026-01-11 17:42:35 +0100

    Feat: tighter global validation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({
       whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
     }),
   );
   app.setGlobalPrefix('api');
