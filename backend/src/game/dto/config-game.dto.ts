58aa6406aeffe42d4e2d01cfb683f9571acfa2f4
mdomnik <mdomnik.maciej@gmail.com>
2026-01-13 16:23:49 +0100

    Feat: Added Game Gateways

---
+++ backend/src/game/dto/config-game.dto.ts
@@ -0,0 +1,43 @@
+import {
+  IsString,
+  IsInt,
+  IsNotEmpty,
+  IsUUID,
+  Min,
+  Max,
+  MaxLength,
+  ValidateNested,
+  ArrayMinSize,
+  ArrayMaxSize,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class ConfigDto {
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(20)
+  roundTotal: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(20)
+  @Max(120)
+  timePerQuestion: number;
+
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(10)
+  questionsPerRound: number;
+}
+
+
+export class ConfigGameDto {
+    
+    @IsUUID()
+    lobbyId: string;
+
+    config: ConfigDto;
+}
